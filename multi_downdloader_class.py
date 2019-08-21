e29f5f748465801fb6ef9f86cf0ff5706e9182e7
Vladimir <angara99@gmail.com>
2019-08-21 19:00:30 +0300

    All working good! Finaly

--- multi_downdloader_class.py
+++ multi_downdloader_class.py
@@ -9,27 +9,28 @@ import sys
 from random import shuffle
 import configparser
 import multiprocessing 
+from multiprocessing import Pool
 
+config = configparser.ConfigParser()
+config.read('config.cnf')
+host = config['DEFAULT']['host']
+user = config['DEFAULT']['user']
+password = config['DEFAULT']['password']
+database = config['DEFAULT']['database']
+db = MySQLdb.connect(host, user, password, database)
+db.set_character_set('utf8')
 class ImageDownloader():
 
-    config = configparser.ConfigParser()
-    config.read('config.cnf')
-    host = config['DEFAULT']['host']
-    user = config['DEFAULT']['user']
-    password = config['DEFAULT']['password']
-    database = config['DEFAULT']['database']
     num = 0
     #limit = 1000    #Limit for testing
 
     
-    db = MySQLdb.connect(host, user, password, database)
-    db.set_character_set('utf8')
     #cursor = db.cursor()
     
     def __init__(self, table = 'product_allegro_back2',limit=100, *args):
         self.table = table
         self.limit = limit
-        self.proxy_count = self.proxy_count()
+        self.proxy_count = self.proxy_count()//3
 
     def proxy_count(self):
         with open('proxy.txt', 'r') as f:
@@ -42,12 +43,11 @@ class ImageDownloader():
         array = [ x.strip() for x in array ]
         return([pk, array])
 
-    def iter_row(self, size=1000):
-        db = MySQLdb.connect(self.host, self.user, self.password, self.database)
-        db.set_character_set('utf8')
+    def iter_row(self, size):
         cursor = db.cursor()
         #Getting data from table to translate
-        q = f'SELECT id, subcat_id  FROM {self.table} WHERE (subsubcat_id = "" OR subsubcat_id ="None" OR LENGTH(subsubcat_id) < 5) AND LENGTH(subcat_id) > 5  LIMIT 101000,{self.limit}'
+        q = f'SELECT id, subcat_id  FROM {self.table} WHERE (subsubcat_id = "" OR subsubcat_id ="None" OR\
+                LENGTH(subsubcat_id) < 5) AND LENGTH(subcat_id) > 5  LIMIT 0,{self.limit}'
         cursor.execute(q)
         while True:
             rows = cursor.fetchmany(size)
@@ -78,20 +78,19 @@ class ImageDownloader():
         for i, row in enumerate(self.iter_row(50)):
             print(row, i)
 
-    def do_translate_job(self):
+    def do_translate_job(self, rows_list):
         #iter_rows Выбирает из бд количество строк и выдает их по пучку в каждый процесс
         with progressbar.ProgressBar(max_value=self.limit) as bar:    
-            for i, row in enumerate(self.iter_row(self.limit//self.proxy_count)): #Задаем количество строк в банче
-                p = multiprocessing.Process(target=self.translator, args=(row,))
-                p.start()
+            for i, row in enumerate(rows_list): #Задаем количество строк в банче
+                self.translator(rows_list)
                 bar.update(i)
-        print("Rows affected:", self.num)
-        self.db.commit()
         return(self.num)
         
     def translator(self, trans_list):
-        
-        cursor = self.db.cursor()
+
+        db = MySQLdb.connect(host, user, password, database)
+        db.set_character_set('utf8')
+        cursor = db.cursor()
         proxy = next(self.proxy_list())
         user_agent = next(self.user_agent())
         url = 'https://www.webtran.ru/gtranslate/'
@@ -104,18 +103,16 @@ class ImageDownloader():
                }
                 headers = {'User-Agent': user_agent}
                 try:
-                    print('responsing')
                     response = requests.post(url, data=data, headers=headers,timeout=10, proxies={'http': proxy, 'https': proxy})
                     time.sleep(4)
+
                     if len(response.text)>5:
                         text = response.text
-
                     else:
                         text = 'None'
 
                     qu = f'UPDATE {self.table} SET subsubcat_id = %s WHERE id = %s'
                     cursor.execute(qu, (text, r[0]))
-                    self.db.commit()
                     if cursor.rowcount == 0:
                         print('Not inserted')
                     else:
@@ -127,13 +124,30 @@ class ImageDownloader():
                     proxy = next(self.proxy_list())
                     user_agent = next(self.proxy_list())
                 bar.update(i)         
-#            cursor.close()
-#            db.close()
+            db.commit()
+            cursor.close()
+            db.close()
 
+def do_all():
+    limit = 800000
+    processes = 40
+    chunk = limit // processes
+
+    imdow = ImageDownloader('product_allegro_back2', limit)
+    with Pool(processes) as p:
+        var = p.map(imdow.translator, imdow.iter_row(chunk) )
+#    with progressbar.ProgressBar(max_value=10) as bar:
+#        for i, row in enumerate(imdow.iter_row(10)):
+#            p = multiprocessing.Process(target=imdow.translator, args=(row,))
+#            p.start()
+#            bar.update(i)
+        p.close()
+        p.join()
+    db.close()
+    print(f'Connection Closed', imdow.num) 
 
 if __name__ == '__main__':
-    
-    imdow = ImageDownloader('product_allegro_back2', 100000)
-    num = imdow.do_translate_job()
-    #imdow.db.close()
-    print(f'Connection Closed inserted {num}')
+    t1 = time.time()
+    do_all() 
+    t2 = time.time()
+    print("Total tim :", (t2 - t1)/60, 'Minutes')
