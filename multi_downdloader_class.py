bb1a9e6e8d1e137debd498024e5d60122f05767a
Vladimir <angara99@gmail.com>
2019-08-23 18:06:03 +0300

    small addition to visualize the work

--- multi_downdloader_class.py
+++ multi_downdloader_class.py
@@ -17,8 +17,7 @@ host = config['DEFAULT']['host']
 user = config['DEFAULT']['user']
 password = config['DEFAULT']['password']
 database = config['DEFAULT']['database']
-db = MySQLdb.connect(host, user, password, database)
-db.set_character_set('utf8')
+#db = MySQLdb.connect(host, user, password, database)
 class ImageDownloader():
 
     num = 0
@@ -27,13 +26,14 @@ class ImageDownloader():
     
     #cursor = db.cursor()
     
-    def __init__(self, table = 'product_allegro_back2',limit=100, *args):
+    def __init__(self, table = 'product_allegro_back2',limit=100, *args, **kwargs):
         self.table = table
         self.limit = limit
+        self.prox_lst = kwargs['proxy_list']
         self.proxy_count = self.proxy_count()//3
 
     def proxy_count(self):
-        with open('proxy.csv', 'r') as f:
+        with open(self.prox_lst, 'r') as f:
             reader = csv.reader(f)
             proxy_list = list(reader)
             return len(proxy_list)
@@ -44,19 +44,22 @@ class ImageDownloader():
         return([pk, array])
 
     def iter_row(self, size):
-        cursor = db.cursor()
+        db_iter = MySQLdb.connect(host, user, password, database)
+        db_iter.set_character_set('utf8')
+        cursor_iter = db_iter.cursor()
         #Getting data from table to translate
         q = f'SELECT id, subcat_id  FROM {self.table} WHERE (subsubcat_id = "" OR subsubcat_id ="None" OR\
                 LENGTH(subsubcat_id) < 5) AND LENGTH(subcat_id) > 5  LIMIT 0,{self.limit}'
-        cursor.execute(q)
+        cursor_iter.execute(q)
         while True:
-            rows = cursor.fetchmany(size)
+            rows = cursor_iter.fetchmany(size)
             if not rows:
                 break
             yield (rows)
+        cursor_iter.close()
 
     def proxy_list(self):            
-        with open('proxy.csv', 'r') as f:
+        with open(self.prox_lst, 'r') as f:
             reader = csv.reader(f)
             proxy_list = list(reader)
             self.proxy_count = len(proxy_list)
@@ -87,7 +90,9 @@ class ImageDownloader():
         return(self.num)
         
     def translator(self, trans_list):
-
+        not_inserted = 0
+        print("Length of chunk: ", len(trans_list))
+        time.sleep(5)
         db = MySQLdb.connect(host, user, password, database)
         db.set_character_set('utf8')
         cursor = db.cursor()
@@ -114,26 +119,40 @@ class ImageDownloader():
                     qu = f'UPDATE {self.table} SET subsubcat_id = %s WHERE id = %s'
                     cursor.execute(qu, (text, r[0]))
                     if cursor.rowcount == 0:
-                        print('Not inserted')
+                       not_inserted += 1 
                     else:
                         self.num += cursor.rowcount
                     #print(response.text)
                 except Exception as e:
                     print(e)
                     time.sleep(5)
+                    #rchk = requests('http://angara77.com', timeout=2)
+                    #if rchk.status_code != 200:
                     proxy = next(self.proxy_list())
                     user_agent = next(self.proxy_list())
                 bar.update(i)         
                 db.commit()
             cursor.close()
             db.close()
-
+        return not_inserted
 def do_all():
-    limit = 400000
-    processes = 50
-    chunk = limit // processes
 
-    imdow = ImageDownloader('product_allegro_back2', limit)
+    limit = 100000
+    processes = 50
+    table = 'product_allegro_back2' 
+    q = f'SELECT COUNT(id) FROM {table} WHERE (subsubcat_id = "" OR subsubcat_id ="None" OR\
+ LENGTH(subsubcat_id) < 5) AND LENGTH(subcat_id) > 5  LIMIT 0,{limit}'
+    db2 = MySQLdb.connect(host, user, password, database)
+    cursor2 = db2.cursor() 
+    cursor2.execute(q)
+    count = cursor2.fetchall()
+    print(f"Working whith {count[0][0]} items ")
+    chunk = count[0][0] // processes
+    print("Size of chunk: ", chunk)
+    time.sleep(10)
+    cursor2.close()
+
+    imdow = ImageDownloader(table, limit, proxy_list='proxy_23.csv')
     with Pool(processes) as p:
         var = p.map(imdow.translator, imdow.iter_row(chunk) )
 #    with progressbar.ProgressBar(max_value=10) as bar:
@@ -143,9 +162,9 @@ def do_all():
 #            bar.update(i)
         p.close()
         p.join()
-    db.close()
+    #db.close()
     print(f'Connection Closed', imdow.num) 
-
+    print("Not inserted:", var)
 if __name__ == '__main__':
     t1 = time.time()
     do_all() 
